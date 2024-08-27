99e19280650730a3ed283528af10b908a06a634c
brunoscheifer <brunoscheifer11@gmail.com>
2024-08-27 17:28:08 -0300

    mais um

--- Modulo8/api/index.js
+++ Modulo8/api/index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!!!')
 })
 
+app.get('/outro', (req, res) => {
+    res.send('Outra coisa')
+})
+
 app.listen(port , () => {
     console.log(`Porta ${port}`)
 })
