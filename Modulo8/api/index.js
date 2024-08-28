f5e74faf5b5b4fb2ba13c96dae683cbd3adbd1ea
brunoscheifer <brunoscheifer11@gmail.com>
2024-08-28 17:07:22 -0300

    mudança denovo

--- Modulo8/api/index.js
+++ Modulo8/api/index.js
@@ -2,20 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!!!')
+
+app.use((req, res, next) => {
+    const authHeader = req.headers['authorization']
+    if(!authHeader) {
+        res.status(403).send('Acesso negado: Token invalido!.')
+        throw new Error('Token Invalido')
+    } else {
+        next()
+    }
 })
 
-app.get('/outro', (req, res) => {
-    res.send('Boas vindas voce me achou')
-})
-
-app.get('/info', (req, res) => {
-    res.send('Esse site foi feito usando express!')
-})
-
-app.get('*', (req, res) => {
-    res.status(404).send('Pagina não encontrada')
+app.get('/protected', (req, res) => {
+    if(req.headers['authorization'] == 'senhasecreta') {
+        res.send('Bem Vindo à área protegida!!')
+    } else {
+        res.status(403).send('Acesso negado: Token invalido!')
+        throw new Error('Token Invalido')
+    }
 })
 
 app.listen(port , () => {
